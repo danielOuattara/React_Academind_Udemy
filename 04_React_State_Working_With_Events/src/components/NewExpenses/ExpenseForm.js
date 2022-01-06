5745df077ef81a79c1356e4765335e6bb7834ab4
daniel ouattara <daniel.ouattara@gmx.com>
2022-01-06 23:41:31 +0100

    handleSubmit + 2 ways binding

--- 04_React_State_Working_With_Events/src/components/NewExpenses/ExpenseForm.js
+++ 04_React_State_Working_With_Events/src/components/NewExpenses/ExpenseForm.js
@@ -1,25 +1,126 @@
+import { useState } from 'react';
+import './../../styles/expenseForm.css';
+
+function ExpenseForm() {
+
+    const [ title, setTitle] = useState('');
+    const [ amount, setAmount] = useState('');
+    const [ date, setDate] = useState('');
+
+    const handleExpenseTitle = (event) => {
+        // setTitle(event.target.value);
+        setTitle(() => {
+            return event.target.value
+        });
+    }
+
+    const handleExpenseAmount = (event) => {
+        // setAmount(event.target.value);
+        setAmount(() => {
+            return event.target.value
+        });
+    }
+
+    const handleExpenseDate = (event) => {
+        // setDate(event.target.value);
+        setDate(() => {
+            return event.target.value
+        });
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const expenseData = {title, amount, date: new Date(date) };
+
+        setTitle('');
+        setAmount('');
+        setDate('');
+
+    }
+
+    
+    return (
+        <form>
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
+                    <label htmlFor="title">Title</label>
+                    <input 
+                       type="text" 
+                       value={title} 
+                       onChange={handleExpenseTitle} 
+                    />
+                </div>
+                <div className="new-expense__control">
+                    <label htmlFor="amount">Amount</label>
+                    <input 
+                       type="number" 
+                       value={amount} 
+                       min="0.01" 
+                       step="0.01" 
+                       onChange={handleExpenseAmount} 
+                    />
+                </div>
+                <div className="new-expense__control">
+                    <label htmlFor="date">Date</label>
+                    <input 
+                       type="date" 
+                       value={date} 
+                       min="2019-01-01" 
+                       max="2022-12-31" 
+                       onChange={handleExpenseDate} 
+                    />
+                </div>
+            </div>
+            <div className="new-expense__actions">
+                <button type='submit' onSubmit={handleSubmit}>Add Expense</button>
+            </div>
+        </form>
+        );
+}
+
+export default ExpenseForm;
+
+//------------------------------------------------------------------
+/* One Unique useState() to handle all data, that's is your preference !  */
+
 // import { useState } from 'react';
 // import './../../styles/expenseForm.css';
 
 // function ExpenseForm() {
 
-//     const [ title, setTitle] = useState('');
-//     const [ amount, setAmount] = useState('');
-//     const [ date, setDate] = useState('');
+//     const [userInput, setUserInput] = useState({
+//             title: '',
+//             amount: '',
+//             date: ''
+//         })
 
 //     const handleExpenseTitle = (event) => {
-//         setTitle(event.target.value);
+//         setUserInput( (prevState) => { // good for Async from React itself
+//             return {
+//                 ...prevState,
+//                 title: event.target.value
+//             }            
+//         });
 //     }
 
 //     const handleExpenseAmount = (event) => {
-//         setAmount(event.target.value);
+//         setUserInput((prevState) => { // good for Async from React itself
+//             return {
+//                 ...prevState,
+//                 amount: event.target.value
+//             }
+//         });
 //     }
-
-//     const handleExpenseDate = (event) => {
-//         setDate(event.target.value);
-//     }
-
     
+//     const handleExpenseDate = (event) => {
+//         setUserInput((prevState) => { // good for Async from React itself
+//             return {
+//                 ...prevState,
+//                 date: event.target.value
+//             }
+//         });
+//     }
+
 //     return (
 //         <form>
 //             <div className="new-expense__controls">
@@ -27,7 +128,7 @@
 //                     <label htmlFor="title">Title</label>
 //                     <input 
 //                        type="text" 
-//                        value={title} 
+//                        value={userInput.title} 
 //                        onChange={handleExpenseTitle} 
 //                     />
 //                 </div>
@@ -35,7 +136,7 @@
 //                     <label htmlFor="amount">Amount</label>
 //                     <input 
 //                        type="number" 
-//                        value={amount} 
+//                        value={userInput.amount} 
 //                        min="0.01" 
 //                        step="0.01" 
 //                        onChange={handleExpenseAmount} 
@@ -45,7 +146,7 @@
 //                     <label htmlFor="date">Date</label>
 //                     <input 
 //                        type="date" 
-//                        value={date} 
+//                        value={userInput.date} 
 //                        min="2019-01-01" 
 //                        max="2022-12-31" 
 //                        onChange={handleExpenseDate} 
@@ -58,86 +159,4 @@
 //         </form>
 //         );
 // }
-
 // export default ExpenseForm;
-
-//------------------------------------------------------------------
-/* One Unique useState() to handle all data, that's is your preference !  */
-
-import { useState } from 'react';
-import './../../styles/expenseForm.css';
-
-function ExpenseForm() {
-
-    const [userInput, setUserInput] = useState({
-            title: '',
-            amount: '',
-            date: ''
-        })
-
-    const handleExpenseTitle = (event) => {
-        setUserInput( (prevState) => { // good for Async from React itself
-            return {
-                ...prevState,
-                title: event.target.value
-            }            
-        });
-    }
-
-    const handleExpenseAmount = (event) => {
-        setUserInput((prevState) => { // good for Async from React itself
-            return {
-                ...prevState,
-                amount: event.target.value
-            }
-        });
-    }
-    
-    const handleExpenseDate = (event) => {
-        setUserInput((prevState) => { // good for Async from React itself
-            return {
-                ...prevState,
-                date: event.target.value
-            }
-        });
-    }
-
-    return (
-        <form>
-            <div className="new-expense__controls">
-                <div className="new-expense__control">
-                    <label htmlFor="title">Title</label>
-                    <input 
-                       type="text" 
-                       value={userInput.title} 
-                       onChange={handleExpenseTitle} 
-                    />
-                </div>
-                <div className="new-expense__control">
-                    <label htmlFor="amount">Amount</label>
-                    <input 
-                       type="number" 
-                       value={userInput.amount} 
-                       min="0.01" 
-                       step="0.01" 
-                       onChange={handleExpenseAmount} 
-                    />
-                </div>
-                <div className="new-expense__control">
-                    <label htmlFor="date">Date</label>
-                    <input 
-                       type="date" 
-                       value={userInput.date} 
-                       min="2019-01-01" 
-                       max="2022-12-31" 
-                       onChange={handleExpenseDate} 
-                    />
-                </div>
-            </div>
-            <div className="new-expense__actions">
-                <button type='submit'>Add Expense</button>
-            </div>
-        </form>
-        );
-}
-export default ExpenseForm;
