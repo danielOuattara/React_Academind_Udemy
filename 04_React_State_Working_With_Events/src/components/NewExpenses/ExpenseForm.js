5bfbf0881072efb6c0af3de108bd77c004b8eeaa
daniel ouattara <daniel.ouattara@gmx.com>
2022-01-06 22:58:44 +0100

    One Unique useState() to handle all data

--- 04_React_State_Working_With_Events/src/components/NewExpenses/ExpenseForm.js
+++ 04_React_State_Working_With_Events/src/components/NewExpenses/ExpenseForm.js
@@ -1,25 +1,101 @@
+// import { useState } from 'react';
+// import './../../styles/expenseForm.css';
+
+// function ExpenseForm() {
+
+//     const [ title, setTitle] = useState('');
+//     const [ amount, setAmount] = useState('');
+//     const [ date, setDate] = useState('');
+
+//     const handleExpenseTitle = (event) => {
+//         setTitle(event.target.value);
+//     }
+
+//     const handleExpenseAmount = (event) => {
+//         setAmount(event.target.value);
+//     }
+
+//     const handleExpenseDate = (event) => {
+//         setDate(event.target.value);
+//     }
+
+    
+//     return (
+//         <form>
+//             <div className="new-expense__controls">
+//                 <div className="new-expense__control">
+//                     <label htmlFor="title">Title</label>
+//                     <input 
+//                        type="text" 
+//                        value={title} 
+//                        onChange={handleExpenseTitle} 
+//                     />
+//                 </div>
+//                 <div className="new-expense__control">
+//                     <label htmlFor="amount">Amount</label>
+//                     <input 
+//                        type="number" 
+//                        value={amount} 
+//                        min="0.01" 
+//                        step="0.01" 
+//                        onChange={handleExpenseAmount} 
+//                     />
+//                 </div>
+//                 <div className="new-expense__control">
+//                     <label htmlFor="date">Date</label>
+//                     <input 
+//                        type="date" 
+//                        value={date} 
+//                        min="2019-01-01" 
+//                        max="2022-12-31" 
+//                        onChange={handleExpenseDate} 
+//                     />
+//                 </div>
+//             </div>
+//             <div className="new-expense__actions">
+//                 <button type='submit'>Add Expense</button>
+//             </div>
+//         </form>
+//         );
+// }
+
+// export default ExpenseForm;
+
+//------------------------------------------------------------------
+/* One Unique useState() to handle all data, that's is your preference !  */
+
 import { useState } from 'react';
 import './../../styles/expenseForm.css';
 
 function ExpenseForm() {
 
-    const [ title, setTitle] = useState('');
-    const [ amount, setAmount] = useState('');
-    const [ date, setDate] = useState('');
+    const [userInput, setUserInput] = useState({
+            title: '',
+            amount: '',
+            date: ''
+        })
 
     const handleExpenseTitle = (event) => {
-        setTitle(event.target.value);
+        setUserInput({
+            ...userInput,
+            title: event.target.value
+        });
     }
 
     const handleExpenseAmount = (event) => {
-        setAmount(event.target.value);
+        setUserInput({
+            ...userInput,
+            amount: event.target.value
+        });
     }
-
-    const handleExpenseDate = (event) => {
-        setDate(event.target.value);
-    }
-
     
+    const handleExpenseDate = (event) => {
+        setUserInput({
+            ...userInput,
+            date: event.target.value
+        });
+    }
+
     return (
         <form>
             <div className="new-expense__controls">
@@ -27,7 +103,7 @@ function ExpenseForm() {
                     <label htmlFor="title">Title</label>
                     <input 
                        type="text" 
-                       value={title} 
+                       value={userInput.title} 
                        onChange={handleExpenseTitle} 
                     />
                 </div>
@@ -35,7 +111,7 @@ function ExpenseForm() {
                     <label htmlFor="amount">Amount</label>
                     <input 
                        type="number" 
-                       value={amount} 
+                       value={userInput.amount} 
                        min="0.01" 
                        step="0.01" 
                        onChange={handleExpenseAmount} 
@@ -45,7 +121,7 @@ function ExpenseForm() {
                     <label htmlFor="date">Date</label>
                     <input 
                        type="date" 
-                       value={date} 
+                       value={userInput.date} 
                        min="2019-01-01" 
                        max="2022-12-31" 
                        onChange={handleExpenseDate} 
@@ -58,5 +134,4 @@ function ExpenseForm() {
         </form>
         );
 }
-
 export default ExpenseForm;
