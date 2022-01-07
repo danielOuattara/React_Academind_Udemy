2927a2e41853c7c78320f61e5a4a60174f33b7a4
daniel ouattara <daniel.ouattara@gmx.com>
2022-01-07 14:59:26 +0100

    conditional redering 3 methods

--- 05_Rendering_Listsand_Conditional_Content/course/src/components/Expenses/Expenses.js
+++ 05_Rendering_Listsand_Conditional_Content/course/src/components/Expenses/Expenses.js
@@ -1,3 +1,91 @@
+// import { useState } from 'react';
+// import './../../styles/expenses.css';
+// import Card from './../UI/Card';
+
+// import ExpenseItem from './ExpenseItem';
+// import ExpensesFilter from './ExpensesFilter';
+
+// function Expenses({expenses}) {
+
+//     const [year, setYear ] = useState("");
+    
+//     const handleFilteredYear = (yearReceived) => {
+//         setYear(() => {
+//             return yearReceived;
+//         });
+//     }
+//     let filteredExpenses = year === "" ? expenses : expenses.filter(item => item.date.getFullYear() === Number(year));
+//     return (
+//         <div>
+//             <Card className="expenses">
+//                 <ExpensesFilter handleFilteredYear={handleFilteredYear}/>
+//                 { filteredExpenses.length === 0 ?  
+//                     <h2 style={{color: "white"}}>No Data for {year}</h2> 
+//                     : 
+//                     filteredExpenses.map((item) => {
+//                         return ( 
+//                             <ExpenseItem 
+//                                 key={item.id} 
+//                                 title={item.title} 
+//                                 date={item.date} 
+//                                 amount={item.amount}
+//                             />
+//                         );
+//                     })} 
+//             </Card>
+//         </div>
+//     );
+// }
+
+// export default Expenses;
+
+//-----------------------------------------------------------------------
+// 
+
+// import { useState } from 'react';
+// import './../../styles/expenses.css';
+// import Card from './../UI/Card';
+
+// import ExpenseItem from './ExpenseItem';
+// import ExpensesFilter from './ExpensesFilter';
+
+// function Expenses({expenses}) {
+
+//     const [year, setYear ] = useState("");
+    
+//     const handleFilteredYear = (yearReceived) => {
+//         setYear(() => {
+//             return yearReceived;
+//         });
+//     }
+//     let filteredExpenses = year === "" ? expenses : expenses.filter(item => item.date.getFullYear() === Number(year));
+//     return (
+//         <div>
+
+//             <Card className="expenses">
+//                 <ExpensesFilter handleFilteredYear={handleFilteredYear}/>
+//                 { filteredExpenses.length === 0 && <h2 style={{color: "white"}}>No Data for {year}</h2> } 
+//                 { filteredExpenses.length > 0 && filteredExpenses.map((item) => {
+//                         return ( 
+//                             <ExpenseItem 
+//                                 key={item.id} 
+//                                 title={item.title} 
+//                                 date={item.date} 
+//                                 amount={item.amount}
+//                             />
+//                         );
+//                     })
+//                 } 
+//             </Card>
+//         </div>
+//     );
+// }
+
+// export default Expenses;
+
+
+//-----------------------------------------------------------------------
+
 import { useState } from 'react';
 import './../../styles/expenses.css';
 import Card from './../UI/Card';
@@ -14,23 +102,30 @@ function Expenses({expenses}) {
             return yearReceived;
         });
     }
-    let filteredExpenses = year === "" ? expenses : expenses.filter(item => item.date.getFullYear() === Number(year));
+    let filteredExpenses = year === "" ? 
+        expenses 
+        : 
+        expenses.filter(item => item.date.getFullYear() === Number(year));
+
+    let expensesContent = <h2 style={{color: "white"}}>No Data for {year}</h2>;
+    if(filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((item) => {
+            return ( 
+                <ExpenseItem 
+                    key={item.id} 
+                    title={item.title} 
+                    date={item.date} 
+                    amount={item.amount}
+                />
+            );
+        });
+    }
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter handleFilteredYear={handleFilteredYear}/>
-                { filteredExpenses.length > 0 && filteredExpenses.map((item) => {
-                    return ( 
-                        <ExpenseItem 
-                            key={item.id} 
-                            title={item.title} 
-                            date={item.date} 
-                            amount={item.amount}
-                        />
-                    );
-                })} 
-
-                { filteredExpenses.length === 0 && <h2 style={{color: "white"}}>No Data for {year}</h2>} 
+                { expensesContent }
+ 
             </Card>
         </div>
     );
