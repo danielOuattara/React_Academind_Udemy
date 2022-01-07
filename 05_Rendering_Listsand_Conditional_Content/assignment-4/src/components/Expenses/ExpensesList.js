32613e546d6e92b99bf48747c6acfb744dbef966
daniel ouattara <daniel.ouattara@gmx.com>
2022-01-07 15:28:07 +0100

    satrting assignment-4

---
+++ 05_Rendering_Listsand_Conditional_Content/assignment-4/src/components/Expenses/ExpensesList.js
@@ -0,0 +1,27 @@
+import './../../styles/expensesList.css';
+import ExpenseItem from './ExpenseItem';
+
+
+function ExpensesList({ filteredExpenses, year}) {
+
+    if(filteredExpenses.length === 0) {
+        return <h2 className='expenses-list__fallback'>No Expenses Found for {year}</h2>
+    }
+
+    return (
+        <ul className='expense-list'>
+            {filteredExpenses.map((item) => {
+                return ( 
+                    <ExpenseItem 
+                        key={item.id} 
+                        title={item.title} 
+                        date={item.date} 
+                        amount={item.amount}
+                    />
+                );
+            })}
+        </ul>
+    );
+}
+
+export default ExpensesList;
