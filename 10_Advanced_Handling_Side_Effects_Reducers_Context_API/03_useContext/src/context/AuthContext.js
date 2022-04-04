4535d94fbdbc663c70afcb8991df6049fe10e320
daniel ouattara <daniel.ouattara@gmx.com>
2022-04-04 11:30:30 +0200

    contextAPI with __.Provider & __.Consummer

---
+++ 10_Advanced_Handling_Side_Effects_Reducers_Context_API/03_useContext/src/context/AuthContext.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AuthContext = React.createContext({
+  isLoggedIn: false,
+});
+
+export default AuthContext;
+
+// function auhtContext() {
+//   return <div>auhtContext</div>;
+// }
+
+// export default auhtContext;
