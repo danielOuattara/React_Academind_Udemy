7cf74409a28b4aac381ba1e58de0a921ae720a6c
daniel ouattara <daniel.ouattara@gmx.com>
2022-04-05 14:54:48 +0200

    useCallback to prevent unnecessary renders, completes React.memo

--- 12_Behind_The_Scene_React_Optimization_Strategies/src/App.js
+++ 12_Behind_The_Scene_React_Optimization_Strategies/src/App.js
@@ -40,7 +40,33 @@
 // export default App;
 //-------------------------------------------------------------------
 
-import React, { useState } from "react";
+// import React, { useState } from "react";
+// import "./App.css";
+// import DemoOutput from "./components/Demo/DemoOutput";
+// import Button from "./components/UI/Button/Button";
+
+// function App() {
+//   const [showParagraph, setShowParagraph] = useState(false);
+//   console.log("APP RUNNING");
+//   return (
+//     <div className="app">
+//       <h1>Hi there!</h1>
+//       <Button onClick={() => setShowParagraph(!showParagraph)}>
+//         Toggle Paragraph
+//       </Button>
+//       <DemoOutput /> {/* See in console */}
+//     </div>
+
+//     /* It is enough for a function in a child component to be re-evaluate if the
+//        the parent component is re-evaluated  */
+//   );
+// }
+
+// export default App;
+
+//-------------------------------------------------------------------
+
+import React, { useState, useCallback } from "react";
 
 import "./App.css";
 import DemoOutput from "./components/Demo/DemoOutput";
@@ -48,19 +74,37 @@ import Button from "./components/UI/Button/Button";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+
   console.log("APP RUNNING");
+
+  const toggleParagraph = useCallback(() => {
+    setShowParagraph((showParagraph) => !showParagraph);
+  }, []);
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <Button onClick={() => setShowParagraph(!showParagraph)}>
-        Toggle Paragraph
-      </Button>
+      {/* This is not sensible to useCallback() */}
+      {/* <Button onClick={() => toggleParagraph()}>Toggle Paragraph</Button> */} 
+      
+      {/* This is working on useCallback */}
+      <Button onClick={toggleParagraph}>Toggle Paragraph</Button>
       <DemoOutput /> {/* See in console */}
     </div>
 
     /* It is enough for a function in a child component to be re-evaluate if the
        the parent component is re-evaluated  */
   );
+
+
+  /*
+useCallback is a React hook. Used on callback functions.
+It will return a memoized version of the callback that 
+change only if one of callback's dependencies changes
+
+Useful for unnecessary renders of callback passed in components
+from a parent component 
+*/
 }
 
 export default App;
