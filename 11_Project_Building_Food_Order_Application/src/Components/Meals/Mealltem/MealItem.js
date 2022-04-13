990f5ca6f3acc46257452f0e06d262901533f650
daniel ouattara <daniel.ouattara@gmx.com>
2022-04-13 22:51:33 +0200

    Adding Input Form

--- 11_Project_Building_Food_Order_Application/src/Components/Meals/Mealltem/MealItem.js
+++ 11_Project_Building_Food_Order_Application/src/Components/Meals/Mealltem/MealItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
 
 function MealItem(props) {
   const price = `$${props.price.toFixed(2)}`;
@@ -10,6 +11,9 @@ function MealItem(props) {
         <h3>{props.name}</h3>
         <div className={styles.description}>{props.description}</div>
         <div className={styles.price}>{price}</div>
+      </div>
+      <div>
+          <MealItemForm />
       </div>
     </li>
   );
