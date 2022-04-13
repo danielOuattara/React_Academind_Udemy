f35c1b120e17ce7e5da06d2f09cd8c8cecfb4885
daniel ouattara <daniel.ouattara@gmx.com>
2022-04-13 22:35:17 +0200

    individual meal display

--- 11_Project_Building_Food_Order_Application/src/Components/Meals/AvailableMeals.js
+++ 11_Project_Building_Food_Order_Application/src/Components/Meals/AvailableMeals.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { DUMMY_MEALS } from "../../data/dummy-meals";
+import Card from "../UI/Card";
 import styles from "./AvailableMeals.module.css";
+import MealItem from "./Mealltem/MealItem";
 
 function AvailableMeals() {
   return (
     <section className={styles.meals}>
-      <ul>
-        {DUMMY_MEALS.map((meal) => {
-          return <li key={meal.id}>{meal.name}</li>;
-        })}
-      </ul>
+      <Card>
+        <ul>
+          {DUMMY_MEALS.map((meal) => {
+            return <MealItem key={meal.id} {...meal} />;
+          })}
+        </ul>
+      </Card>
     </section>
   );
 }
