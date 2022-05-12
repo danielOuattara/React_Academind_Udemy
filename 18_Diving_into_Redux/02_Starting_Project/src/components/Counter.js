f86fb8be789080c681aef03431f93d3f636b5ed9
daniel ouattara <daniel.ouattara@gmx.com>
2022-05-12 08:19:34 +0200

    connecting react & redux functionnal & calass comp

---
+++ 18_Diving_into_Redux/02_Starting_Project/src/components/Counter.js
@@ -0,0 +1,116 @@
+/* 1: using connect form react-redux using functionnal component
+-------------------------------------------------------------- */
+
+// import classes from "./Counter.module.css";
+// import { connect } from "react-redux";
+
+// const Counter = (props) => {
+// console.log(props)
+
+//   const toggleCounterHandler = () => {
+//     props.increment();
+//   };
+
+//   return (
+//     <main className={classes.counter}>
+//       <h1>Redux Counter</h1>
+//       <div className={classes.value}>{props.counter}</div>
+//       <button onClick={toggleCounterHandler}>Toggle Counter</button>
+//     </main>
+//   );
+// };
+
+// const mapStateToProps = (state) => {
+//   return { counter: state.counter };
+// };
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     increment: () => {
+//       dispatch({
+//         type: "INCREMENT",
+//       });
+//     },
+//   };
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+
+/* 2: using useSelctor, useDispatch form react-redux using functionnal component
+------------------------------------------------------------------------------- */
+import classes from "./Counter.module.css";
+import { useSelector, useDispatch } from "react-redux";
+
+const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+  const incrementBy5Handler = () => {
+    dispatch({ type: "INCREMENT", payload: +5 });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: "DECREMENT" });
+  };
+  const toggleCounterHandler = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={decrementHandler}>Decrement</button>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={incrementBy5Handler}>Increment By 5</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
+
+// /* 3:  using connect form react-redux using class component
+// ----------------------------------------------------------- */
+
+// import React, { Component } from "react";
+// import classes from "./Counter.module.css";
+// import { connect } from "react-redux";
+
+// export class Counter extends Component {
+//   render() {
+//     return (
+//       <main className={classes.counter}>
+//         <h1>Redux Counter</h1>
+//         <div className={classes.value}>{this.props.counter}</div>
+//         <div>
+//           <button onClick={this.props.decrementHandler}>Decrement</button>
+//           <button onClick={this.props.incrementHandler}>Increment</button>
+//         </div>
+//         <button onClick={this.props.toggleCounterHandler}>
+//           Toggle Counter
+//         </button>
+//       </main>
+//     );
+//   }
+// }
+
+// const mapStateToProps = (state) => {
+//   return { counter: state.counter };
+// };
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     incrementHandler: () => {
+//       dispatch({ type: "INCREMENT" });
+//     },
+//     decrementHandler: () => {
+//       dispatch({ type: "DECREMENT" });
+//     },
+//   };
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
