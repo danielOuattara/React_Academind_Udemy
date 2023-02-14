eca238737df80c421d593b03e279d5814cfe6cb1
danielOuattara <daniel.ouattara@gmx.com>
2023-02-14 13:25:35 +0100

    update logic

--- 09_Fragments_Portals_Refs/src/components/Users/AddUser.js
+++ 09_Fragments_Portals_Refs/src/components/Users/AddUser.js
@@ -1,3 +1,87 @@
+// import { useState } from "react";
+// import Card from "../UI/Card";
+// import Button from "../UI/Button";
+// import ErrorModal from "../UI/ErrorModal";
+// import classes from "./AddUser.module.css";
+// import Wrapper from "../Helpers/Wrapper";
+
+// const AddUser = (props) => {
+//   const [enteredUsername, setEnteredUsername] = useState("");
+//   const [enteredAge, setEnteredAge] = useState("");
+//   const [error, setError] = useState(null);
+
+//   const addUserHandler = (event) => {
+//     event.preventDefault();
+//     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+//       setError({
+//         title: "Invalid input",
+//         message: "Please enter a valid name and age (non-empty values).",
+//       });
+//       return;
+//     }
+//     if (+enteredAge < 1) {
+//       setError({
+//         title: "Invalid age",
+//         message: "Please enter a valid age (> 0).",
+//       });
+//       return;
+//     }
+//     props.onAddUser(enteredUsername, enteredAge);
+//     setEnteredUsername("");
+//     setEnteredAge("");
+//   };
+
+//   const usernameChangeHandler = (event) => {
+//     setEnteredUsername(event.target.value);
+//   };
+
+//   const ageChangeHandler = (event) => {
+//     setEnteredAge(event.target.value);
+//   };
+
+//   const errorHandler = () => {
+//     setError(null);
+//   };
+
+//   return (
+//     <Wrapper>
+//       {error && (
+//         <ErrorModal
+//           title={error.title}
+//           message={error.message}
+//           onConfirm={errorHandler}
+//         />
+//       )}
+//       <Card className={classes.input}>
+//         <form onSubmit={addUserHandler}>
+//           <label htmlFor="username">Username</label>
+
+//           <input
+//             id="username"
+//             type="text"
+//             value={enteredUsername}
+//             onChange={usernameChangeHandler}
+//           />
+//           <label htmlFor="age">Age (Years)</label>
+//           <input
+//             id="age"
+//             type="number"
+//             value={enteredAge}
+//             onChange={ageChangeHandler}
+//           />
+//           <Button type="submit">Add User</Button>
+//         </form>
+//       </Card>
+//     </Wrapper>
+//   );
+// };
+
+// export default AddUser;
+
+//
+
+//---------------------------- Using ref below
+
 import { useState, useRef } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
@@ -8,15 +92,13 @@ import Wrapper from "../Helpers/Wrapper";
 const AddUser = (props) => {
   const nameInputRef = useRef("");
   const ageInputRef = useRef("");
-  console.log("useRef = ", useRef);
 
-  // const [enteredUsername, setEnteredUsername] = useState("");
-  // const [enteredAge, setEnteredAge] = useState("");
   const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
     console.log(nameInputRef.current.value);
+
     // useRef then make useState non useful: see all commented
 
     // but problem: A component is changing an uncontrolled input
@@ -39,28 +121,15 @@ const AddUser = (props) => {
       return;
     }
     props.onAddUser(userNameInput, userAgeInput);
-    // allows to clear the inputs fiels,
+    // allows to clear the inputs fields,
     // But Avoid !!! Do Not Manipuate The DOM, it's The Job Of ReactDOM
     nameInputRef.current.value = "";
     ageInputRef.current.value = "";
-
-    // setEnteredUsername("");
-    // setEnteredAge("");
   };
-
-  // const usernameChangeHandler = (event) => {
-  //   setEnteredUsername(event.target.value);
-  // };
-
-  // const ageChangeHandler = (event) => {
-  //   setEnteredAge(event.target.value);
-  // };
 
   const errorHandler = () => {
     setError(null);
   };
-
-  // console.log(enteredUsername)
 
   return (
     <Wrapper>
@@ -80,7 +149,6 @@ const AddUser = (props) => {
             id="username"
             type="text"
             value={nameInputRef.current.value}
-            // onChange={usernameChangeHandler}
             ref={nameInputRef}
           />
           <label htmlFor="age">Age (Years)</label>
@@ -89,7 +157,6 @@ const AddUser = (props) => {
             id="age"
             type="number"
             value={ageInputRef.current.value}
-            // onChange={ageChangeHandler}
             ref={ageInputRef}
           />
           <Button type="submit">Add User</Button>
