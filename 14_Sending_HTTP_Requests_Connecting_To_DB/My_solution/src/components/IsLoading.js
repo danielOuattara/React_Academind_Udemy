3dba2bf0ca13ff92e137c5b96a042408e1979a3d
danielOuattara <daniel.ouattara@gmx.com>
2023-04-03 16:43:24 +0200

    my solution complete update

---
+++ 14_Sending_HTTP_Requests_Connecting_To_DB/My_solution/src/components/IsLoading.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./IsLoading.module.css"
+
+
+function IsLoading() {
+  return <p className={styles.isLoading}>Loading...</p>;
+}
+
+export default IsLoading;
