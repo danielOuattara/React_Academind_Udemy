d989a7289410a24314007a98d0feb9528fe5bf49
danielOuattara <daniel.ouattara@gmx.com>
2023-04-06 14:57:54 +0200

    18 Redux: update 01 explore redux

--- 18_Diving_into_Redux/01_Explore_Redux/redux.js
+++ 18_Diving_into_Redux/01_Explore_Redux/redux.js
@@ -1,85 +1,91 @@
 /* Example 1 
 ---------------------- */
 
-const redux = require("redux");
-
-const counterReducer = (state = { counter: 0 }, action) => {
-  if (action.type === "ADD") {
-    return {
-      counter: state.counter + action.payload,
-    };
-  }
-  if (action.type === "SUBSTRACT") {
-    return {
-      counter: state.counter - action.payload,
-    };
-  }
-};
-
-const store = redux.createStore(counterReducer);
-
-const counterSubscriber = () => {
-  console.log("New State", store.getState());
-};
-
-store.subscribe(counterSubscriber);
-
-store.dispatch({
-  type: "ADD",
-  payload: 1,
-});
-store.dispatch({
-  type: "SUBSTRACT",
-  payload: 1,
-});
-store.dispatch({
-  type: "ADD",
-  payload: 1,
-});
-
-/* Example 2 
----------------------- */
-
 // const redux = require("redux");
 
-// // 2
-// const reducer = (state, action) => {
-//   switch (action.type) {
-//     case "ADD":
-//       state = state + action.payload;
-//       break;
-
-//     case "SUBSTRACT":
-//       state = state - action.payload;
-//       break;
-//     default:
-//       return state;
+// function counterReducer(state = { counter: 0 }, action) {
+//   if (action.type === "ADD") {
+//     return {
+//       counter: state.counter + action.payload,
+//     };
 //   }
-//   return state; // ALWAYS return the new state
-// };
 
-// // 1
-// const store = redux.createStore(reducer, 1 /* 1 = initial state*/);
+//   if (action.type === "SUBTRACT") {
+//     return {
+//       counter: state.counter - action.payload,
+//     };
+//   }
 
-// // 4, normaly handled by react.js
-// store.subscribe(() => {
-//   //  fat arrow function get fired when the store is updated
-//   console.log("Store updated ", store.getState());
-// });
+//   return state;
+// }
 
-// // 3
+// const store = redux.createStore(counterReducer);
+
+// function counterSubscriber() {
+//   console.log("New State", store.getState());
+// }
+
+// store.subscribe(counterSubscriber);
+
 // store.dispatch({
-//   // we dispatch action as object
 //   type: "ADD",
-//   payload: +100,
+//   payload: 1,
+// });
+
+// store.dispatch({
+//   type: "SUBTRACT",
+//   payload: 1,
 // });
 
 // store.dispatch({
 //   type: "ADD",
-//   payload: +22,
+//   payload: 1,
 // });
 
-// store.dispatch({
-//   type: "SUBSTRACT",
-//   payload: 80,
-// });
+//
+//
+/* Example 2 ------------------------------------------------------------------ */
+
+const redux = require("redux");
+
+// 2
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD":
+      state = state + action.payload;
+      break;
+
+    case "SUBTRACT":
+      state = state - action.payload;
+      break;
+    default:
+      return state;
+  }
+  return state; // ALWAYS return the new state
+};
+
+// 1
+const store = redux.createStore(reducer, 1 /* 1 = initial state*/);
+
+// 4, normally handled by react.js
+store.subscribe(() => {
+  //  fat arrow function get fired when the store is updated
+  console.log("Store updated ", store.getState());
+});
+
+// 3
+store.dispatch({
+  // we dispatch action as object
+  type: "ADD",
+  payload: +100,
+});
+
+store.dispatch({
+  type: "ADD",
+  payload: +22,
+});
+
+store.dispatch({
+  type: "SUBTRACT",
+  payload: 80,
+});
