5b8a0e586a047448a9f198805acf8d12b192f310
danielOuattara <daniel.ouattara@gmx.com>
2023-04-07 21:16:58 +0200

    20 react_router: 01 adding basic link

--- 20_Building_a_multi_page_with_React_Router/01-starting-project/src/App.jsx
+++ 20_Building_a_multi_page_with_React_Router/01-starting-project/src/App.jsx
@@ -1,7 +1,35 @@
+// import {
+//   createBrowserRouter,
+//   createRoutesFromElements,
+//   Route,
+//   RouterProvider,
+// } from "react-router-dom";
+// import Home from "./pages/Home";
+// import Products from "./pages/Products";
+
+// // declaring routes version 2
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<Home />} />
+//     <Route path="/products" element={<Products />} />
+//   </Route>,
+// );
+
+// const router2 = createBrowserRouter(routeDefinitions);
+
+// export default function App() {
+//   return <RouterProvider router={router2} />;
+// }
+
+//
+// --------------------------------------------------------------------
+//
+
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 
+// declaring route, version 1
 const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "/products", element: <Products /> },
