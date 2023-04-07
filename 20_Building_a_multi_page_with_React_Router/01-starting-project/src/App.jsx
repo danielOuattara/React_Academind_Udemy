ee16837fdaf3e8c5fff28357ad76b88258ad6717
danielOuattara <daniel.ouattara@gmx.com>
2023-04-07 21:03:46 +0200

    20 react_router: 01 adding product page

--- 20_Building_a_multi_page_with_React_Router/01-starting-project/src/App.jsx
+++ 20_Building_a_multi_page_with_React_Router/01-starting-project/src/App.jsx
@@ -1,7 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
+import Products from "./pages/Products";
 
-const router = createBrowserRouter([{ path: "/", element: <Home /> }, {}]);
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/products", element: <Products /> },
+]);
 
 export default function App() {
   return <RouterProvider router={router} />;
