a86f519f531d57669e880663dc23452fb51e198e
danielOuattara <daniel.ouattara@gmx.com>
2023-05-30 23:06:01 +0200

    28: useState hooks

---
+++ 28_Optional_React_Hooks_Summary/hooks-01-starting/src/components/Ingredients/IngredientForm.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+import Card from "../UI/Card";
+import "./IngredientForm.css";
+
+const IngredientForm = React.memo((props) => {
+  const [inputState, setInputState] = useState({ title: "", amount: 0 });
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    // ...
+  };
+
+  return (
+    <section className="ingredient-form">
+      <Card>
+        <form onSubmit={submitHandler}>
+          <div className="form-control">
+            <label htmlFor="title">Name</label>
+            <input
+              type="text"
+              id="title"
+              value={inputState.title}
+              onChange={(event) =>
+                setInputState((prevState) => ({
+                  ...prevState,
+                  title: event.target.value,
+                }))
+              }
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="amount">Amount</label>
+            <input
+              type="number"
+              id="amount"
+              value={inputState.amount}
+              onChange={(event) =>
+                setInputState((prevState) => ({
+                  ...prevState,
+                  amount: event.target.value,
+                }))
+              }
+            />
+          </div>
+          <div className="ingredient-form__actions">
+            <button type="submit">Add Ingredient</button>
+          </div>
+        </form>
+      </Card>
+    </section>
+  );
+});
+
+export default IngredientForm;
